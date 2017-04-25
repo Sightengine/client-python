76797cc7a04c654a2f7cba90c46cfa81f0f3a5ec
agrandiere <agrandiere@intechinfo.fr>
2017-04-25 13:56:34 +0200

    add real test

--- tests.py
+++ tests.py
@@ -2,8 +2,19 @@ import unittest
 from sightengine.client import SightengineClient
 
 class Tests(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_nudity(self):
+        client = SightengineClient('test', 'test')
+        checkNudity = client.check('nudity')
+
+        output = checkNudity.image('https://d3m9459r9kwism.cloudfront.net/img/examples/example5.jpg')
+
+        self.assertEqual('success', output['status'])
+
+        output2 = checkNudity.image('https://incorrectUrl.jpg')
+
+        self.assertEqual('failure', output2['status'])
+        self.assertEqual('media_error', output2['error']['type'])
+
 
 if __name__ == '__main__':
     unittest.main()
