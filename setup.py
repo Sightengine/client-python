36c2d5773145a2023652808cc14811dd77e99477
agrandiere <agrandiere@intechinfo.fr>
2017-07-26 19:52:47 +0200

    change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name = 'sightengine',
   packages = ['sightengine'],
-  version = '1.2.3',
+  version = '1.2.1',
   description = 'Sightengine Python client',
   author = 'Sightengine',
   author_email='support@sightengine.com',
